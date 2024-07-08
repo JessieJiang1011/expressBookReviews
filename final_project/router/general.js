6d3d70c6f4673aa49556afbbbda0bffea1a02b56
JessieJiang1011 <jiangjiaxi1011@gmail.com>
2024-07-08 08:17:14 -0400

    task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,39 +37,59 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    // res.send(books);
-    res.send(JSON.stringify(books,null,4));
+public_users.get('/',async function (req, res) {
+    try {
+        // res.send(books);
+        res.send(JSON.stringify(books, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+public_users.get('/isbn/:isbn',async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+        res.send(books[isbn]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    let result = [];
-    for (let key in books) {
-        if (books[key].author === author) {
-            result.push(books[key]);
+public_users.get('/author/:author',async function (req, res) {
+    try {
+        const author = req.params.author;
+        let result = [];
+        for (let key in books) {
+            if (books[key].author === author) {
+                result.push(books[key]);
+            }
         }
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-    res.send(result);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    let result = [];
-    for (let key in books) {
-        if (books[key].title === title) {
-            result.push(books[key]);
+public_users.get('/title/:title',async function (req, res) {
+    try {
+        const title = req.params.title;
+        let result = [];
+        for (let key in books) {
+            if (books[key].title === title) {
+                result.push(books[key]);
+            }
         }
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-    res.send(result);
 });
 
 //  Get book review
